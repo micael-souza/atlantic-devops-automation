75a48455553bf52bf31b7763e2caeb8401ae7719
micael-souza <micael.souza@lab360.com.br>
2026-05-03 21:39:11 -0300

    ajustes realiados para o terraform ignorar o checksum da AWS no objsect storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,7 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
 }
 
